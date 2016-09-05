2bbc582f72c62c1950324de3d5e04714b773534f
Graham Place <grahamgplace@gmail.com>
2016-09-05 16:36:20 -0700

    Initial project commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cscv352
+CSCV 352 Assignment Site
---
+++ encrypt.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Message: <?php echo $_POST["message"]; ?><br>
+Your shift value is: <?php echo $_POST["shiftval"]; ?>
+
+</body>
+</html>
