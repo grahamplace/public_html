5abacec8e9312ef75917b323e70f2bf8365fce4a
Graham Place <grahamgplace@gmail.com>
2016-09-05 17:16:35 -0700

    first draft of new encryption

--- encrypt.php
+++ encrypt.php
@@ -1,8 +1,10 @@
 <html>
 <body>
 
-Message: <?php echo $_POST["message"]; ?><br>
-Your shift value is: <?php echo $_POST["shiftval"]; ?>
+Original Message: <?php echo $_POST["message"]; ?><br>
+Shift Value: <?php echo $_POST["shiftval"]; ?>
+To Decrypt, Use: <?php echo 26 - $_POST["shiftval"]; ?>
+
 
 </body>
 </html>
