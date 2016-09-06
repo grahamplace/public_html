ec0135c7d9509f96819dd606892438cb98d40400
Graham Place <grahamgplace@gmail.com>
2016-09-05 18:37:56 -0700

    add scripts

---
+++ status.php
@@ -0,0 +1,13 @@
+<html>
+<h1>Assignment 3: Website Status</h1>
+
+<body>
+<?php $cmd = "/home/u20/grahamplace/public_html/print_status"; ?>
+<pre>
+<p><?php passthru($cmd); ?></p>
+</pre>
+<br>
+</body>
+
+<p><a href="index.html">Home</a>
+</html>
