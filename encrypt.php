f17a510f4a453ac3b323e67a9ee74d23c148eb72
Graham Place <grahamgplace@gmail.com>
2016-09-05 17:48:01 -0700

    multi word message fix

--- encrypt.php
+++ encrypt.php
@@ -2,13 +2,9 @@
 <body>
 <p>Original Message: <?php echo $_POST["message"]; ?> </p>
 <p>Shift Value: <?php echo $_POST["shiftval"]; ?></p>
-<?php $cmd = "/home/u20/grahamplace/public_html/encrypt". " " . $_POST["message"] . " " . $_POST["shiftval"]; ?>
+<?php $cmd = "/home/u20/grahamplace/public_html/encrypt". " " . "\"" . $_POST["message"] . "\"" . " " . $_POST["shiftval"]; ?>
 <p>Encrypted Message: <?php passthru($cmd); ?> </p>
 <p>To Decrypt, Use: <?php echo 26 - $_POST["shiftval"]; ?></p>
-
-<p>Passthru Test: <?php passthru("date"); ?></p>
-
-
 
 </body>
 </html>
