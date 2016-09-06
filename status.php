25e1cae728b3bfa705512447a6ed63927fb145f9
Graham Place <grahamgplace@gmail.com>
2016-09-05 21:44:56 -0700

    first try with new status

--- status.php
+++ status.php
@@ -2,7 +2,7 @@
 <h1>Assignment 3: Website Status</h1>
 
 <body>
-<?php $cmd = "/home/u20/grahamplace/public_html/print_status"; ?>
+<?php $cmd = "/home/u20/grahamplace/public_html/date_script.txt"; ?>
 <pre>
 <p><?php passthru($cmd); ?></p>
 </pre>
