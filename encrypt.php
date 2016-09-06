d4f325d7434f41b1fbd4b51dfbd19e6669431939
Graham Place <grahamgplace@gmail.com>
2016-09-05 17:23:05 -0700

    first passthru test

--- encrypt.php
+++ encrypt.php
@@ -1,9 +1,12 @@
 <html>
 <body>
+<p>Original Message: <?php echo $_POST["message"]; ?> </p>
+<p>Shift Value: <?php echo $_POST["shiftval"]; ?></p>
+<p>Encrypted Message: <?php echo $_POST["shiftval"]; ?></p>
+<p>To Decrypt, Use: <?php echo 26 - $_POST["shiftval"]; ?></p>
 
-Original Message: <?php echo $_POST["message"]; ?><br>
-Shift Value: <?php echo $_POST["shiftval"]; ?>
-To Decrypt, Use: <?php echo 26 - $_POST["shiftval"]; ?>
+<p>Passthru Test: <?php passthru("date"); ?></p>
+
 
 
 </body>
