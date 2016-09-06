6c0266d502b04f4449bf273fce8649882bd0a170
Graham Place <grahamgplace@gmail.com>
2016-09-05 17:53:51 -0700

    randomwalk first draft

---
+++ randomwalk.php
@@ -0,0 +1,12 @@
+<html>
+
+<h1>Assignment 5: Random Walk</h1>
+
+<body>
+<?php $cmd = "/home/u20/grahamplace/public_html/randomwalk"; ?>
+<p><?php passthru($cmd); ?> </p>
+<br>
+</body>
+
+<p><a href="index.html">Home</a> <a href="randomwalk.php">New Random Walk</a></p>
+</html>
