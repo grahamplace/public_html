00c0b629fabaae34a3f91831bee91ac948c9b257
Graham Place <grahamgplace@gmail.com>
2016-09-05 17:42:28 -0700

    add encryption functionality to website

--- encrypt.php
+++ encrypt.php
@@ -2,7 +2,7 @@
 <body>
 <p>Original Message: <?php echo $_POST["message"]; ?> </p>
 <p>Shift Value: <?php echo $_POST["shiftval"]; ?></p>
-<p>Encrypted Message: <?php passthru('/home/u20/grahamplace/public_html/encrypt $_POST["shiftval"]'); ?></p>
+<p>Encrypted Message: <?php passthru('/home/u20/grahamplace/public_html/encrypt $_POST["message"] $_POST["shiftval"]'); ?></p>
 <p>To Decrypt, Use: <?php echo 26 - $_POST["shiftval"]; ?></p>
 
 <p>Passthru Test: <?php passthru("date"); ?></p>
